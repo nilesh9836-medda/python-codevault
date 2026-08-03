630d830ff32bb3571fa361a6b74efb2ae0f6c26b
Nilesh Medda <nileshmedda@gmail.com>
2026-08-03 18:14:29 +0530

    feat: add python code to show data types

---
+++ code/basic/DataTypes.py
@@ -0,0 +1,40 @@
+""" DataTypes in Python """
+
+num = 123
+print(type(num).__name__)
+
+l_num = 12345678910123
+print(type(l_num).__name__)
+
+f_num = 123.123
+print(type(f_num).__name__)
+
+d_num = 12345678910.123456
+print(type(d_num).__name__)
+
+comp = 7 + 5j
+print(type(comp).__name__)
+
+letter = 'a'
+print(type(letter).__name__)
+
+word = 'hello'
+print(type(word).__name__)
+
+s = "I love python"
+print(type(s).__name__)
+
+t = True
+print(type(t).__name__)
+
+lst = [1,2,3,4,5]
+print(type(lst).__name__)
+
+tple = (1,2,3,4,5)
+print(type(tple).__name__)
+
+d = {"code": "Python"}
+print(type(d).__name__)
+
+st = {1,2,3,4,5}
+print(type(st).__name__)
