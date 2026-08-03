74f02073b83b135408e3d39b57a2db47b7c177ff
Nilesh Medda <nileshmedda@gmail.com>
2026-08-03 18:18:35 +0530

    feat: add python code to sum two user given numbers

---
+++ code/basic/Sum.py
@@ -0,0 +1,6 @@
+""" Python code to print sum of user given two numbers """
+
+n1 = int(input("Enter first number: "))
+n2 = int(input("Enter second number: "))
+result = n1 + n2
+print("Sum of numbers is: ", result)
