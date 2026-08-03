5f6b2fcbdb7939ab59b32e9352d39a19fe625400
Nilesh Medda <nileshmedda@gmail.com>
2026-08-03 18:13:20 +0530

    feat: add python code to print output

---
+++ code/basic/PrintOutput.py
@@ -0,0 +1,10 @@
+""" Python code to print the output """
+
+name = "Nilesh Medda"
+age = 23
+print(name, age)
+print("Hello, my name is" + name + ". I am " + str(age) + " years old.")
+print("Hello, my name is",name,". I am ",str(age)," years old.")
+print(f"Hello, my name is {name}. I am {age} years old.")
+print("Hello, my name is {}. I am {} years old.".format(name, age))
+print("Hello, my name is %s. I am %d years old." % (name, age))
